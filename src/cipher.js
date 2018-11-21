1d6a46ff111fe65eac497ee68a1789dd9ad3f964
GloryRojas <grojasm94@gmail.com>
2018-11-21 13:00:48 -0500

    Agregando funcion de descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,21 @@
-const inputText = document.getElementById('inpText')
-const inputOffset = document.getElementById('inpOffset')
-const CipherButton = document.getElementById('ButtonEncode')
-const TextEncode = document.getElementById('OutTextEncode')
-
   function Cipher(inText,Offset) {
   var inText2 = inText.toUpperCase()
     for (var i = 0; i < inText2.length; i++) {
       var AscciCode = ""
       var OutText = ""
-      AscciCode += ((inText2.charCodeAt(i) - 65 + Offset) % 26 + 65)
+      AscciCode += ((inText2.charCodeAt(i) - 65 + (Offset%26)) % 26 + 65)
     OutText += String.fromCharCode(AscciCode)
     console.log(OutText);
       }
   }
 
-  function CipherClick () {
-  const cifrado = Cipher(inputText.value , inputOffset)
-  TextEncode = OutText
-  }
+  function DesCipher(inText,Offset) {
+var inText2 = inText.toUpperCase()
+  for (var i = 0; i < inText2.length; i++) {
+    var AscciCode = ""
+    var OutText = ""
+    AscciCode += ((inText2.charCodeAt(i) - 65 - (Offset%26)+26) % 26 + 65)
+  OutText += String.fromCharCode(AscciCode)
+  console.log(OutText);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const inputText = document.getElementById('inpText')
+const inputOffset = document.getElementById('inpOffset')
+const CipherButton = document.getElementById('ButtonEncode')
+const TextEncode = document.getElementById('OutTextEncode')
