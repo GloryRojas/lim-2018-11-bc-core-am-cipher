fb6b71d47d6f340a0f8abffdea806dcf5d228487
GloryRojas <grojasm94@gmail.com>
2018-11-22 12:36:03 -0500

    index.js estructurado con DOM y enlazado a index.html

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,20 @@
-  function Cipher(inText,Offset) {
-  var inText2 = inText.toUpperCase()
-    for (var i = 0; i < inText2.length; i++) {
-      var AscciCode = ""
-      var OutText = ""
-      AscciCode += ((inText2.charCodeAt(i) - 65 + (Offset%26)) % 26 + 65)
-    OutText += String.fromCharCode(AscciCode)
-    console.log(OutText);
-      }
-  }
-
-  function DesCipher(inText,Offset) {
-var inText2 = inText.toUpperCase()
-  for (var i = 0; i < inText2.length; i++) {
-    var AscciCode = ""
-    var OutText = ""
-    AscciCode += ((inText2.charCodeAt(i) - 65 - (Offset%26)+26) % 26 + 65)
-  OutText += String.fromCharCode(AscciCode)
-  console.log(OutText);
+window.cipher = {
+  encode : (string,Offset) => {
+      var string2 = string.toUpperCase()
+      var OutText = ''
+      for (var i = 0; i < string2.length; i++) {
+        AscciCode = ((string2.charCodeAt(i) - 65 + (Offset%26)) % 26 + 65)
+        OutText += String.fromCharCode(AscciCode)
+        }
+      return OutText;
+      } ,
+ decode : (string,Offset) => {
+      var string2 = string.toUpperCase()
+      var OutText = ''
+      for (var i = 0; i < string2.length; i++) {
+        AscciCode = ((string2.charCodeAt(i) - 65 - (Offset%26)+26) % 26 + 65)
+        OutText += String.fromCharCode(AscciCode)
+        }
+      return OutText;
     }
-}
+ };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-const inputText = document.getElementById('inpText')
-const inputOffset = document.getElementById('inpOffset')
-const CipherButton = document.getElementById('ButtonEncode')
-const TextEncode = document.getElementById('OutTextEncode')
+//Para Cifrado
+const encButt = document.getElementById('ButtonEncode');
+
+const cifrAdo = () => {
+  const textIng = document.getElementById('inpText').value;
+  const ofssetIng = document.getElementById('inpOffset').value;
+    const textcif = cipher.encode (textIng , ofssetIng);
+  document.getElementById('OutTextEncode').innerHTML = textcif;
+}
+encButt.addEventListener('click' , cifrAdo)
+
+//Para descifrado
+const decButt = document.getElementById('ButtonDecode');
+
+const descifrAdo = () => {
+  const textIng = document.getElementById('inpText').value;
+  const ofssetIng = document.getElementById('inpOffset').value;
+    const textcif = cipher.decode (textIng , ofssetIng);
+  document.getElementById('OutTextEncode').innerHTML = textcif;
+}
+decButt.addEventListener('click' , descifrAdo)
