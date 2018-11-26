78557b6e1d385dbf84ed0582c765a4d785f455cf
GloryRojas <grojasm94@gmail.com>
2018-11-26 17:12:08 -0500

    Descifrando espacio y diseño CSS

--- src/cipher.js
+++ src/cipher.js
@@ -3,18 +3,26 @@ window.cipher = {
       var inText2 = inText.toUpperCase()
       var OutText = ''
       for (var i = 0; i < inText2.length; i++) {
-        AscciCode = ((inText2.charCodeAt(i) - 65 + (Offset%26)) % 26 + 65)
-        OutText += String.fromCharCode(AscciCode)
-        }
+        if (inText2[i] === ' '){
+          OutText += ' ';
+        } else {
+          AscciCode = ((inText2.charCodeAt(i) - 65 + (Offset%26)) % 26 + 65);
+          OutText += String.fromCharCode(AscciCode);
+          }
+      }
       return OutText;
       } ,
  decode : (inText,Offset) => {
       var inText2 = inText.toUpperCase()
       var OutText = ''
       for (var i = 0; i < inText2.length; i++) {
-        AscciCode = ((inText2.charCodeAt(i) - 65 - (Offset%26)+26) % 26 + 65)
-        OutText += String.fromCharCode(AscciCode)
-        }
+        if (inText2[i] === ' '){
+          OutText += ' ';
+        } else {
+          AscciCode = ((inText2.charCodeAt(i) - 65 - (Offset%26)+26) % 26 + 65);
+          OutText += String.fromCharCode(AscciCode);
+          }
+      }
       return OutText;
     }
  };
